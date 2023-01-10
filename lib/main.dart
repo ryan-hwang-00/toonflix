9201b34bae0a6e33338bef3b01cbbdb4dd8d07dc
Ryan ingyu hwang <dlsrb07@gmail.com>
2023-01-10 18:02:09 +0900

    webtoon 6.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 
 void main() {
-  runApp(App());
+  ApiService().getTodaysToons();
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+    // TODO: implement build
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,156 +1,25 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  bool isRunning = false;
-  int totalPomodoros = 0;
-  late Timer timer;
-
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        totalPomodoros += 1;
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-      });
-    } else {
-      setState(() {
-        totalSeconds = totalSeconds - 1;
-      });
-    }
-  }
-
-  void onStartPressed() {
-    timer = Timer.periodic(
-      const Duration(seconds: 1),
-      onTick,
-    );
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void onRefreshPressed() {
-    timer.cancel();
-    setState(() {
-      // totalPomodoros += 1;
-      isRunning = false;
-      totalSeconds = twentyFiveMinutes;
-    });
-  }
-
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2, 7);
-  }
-
-  @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        body: Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  format(totalSeconds),
-                  style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 89,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          iconSize: 120,
-                          color: Theme.of(context).cardColor,
-                          onPressed:
-                              isRunning ? onPausePressed : onStartPressed,
-                          icon: Icon(
-                            isRunning
-                                ? Icons.pause_circle_outline
-                                : Icons.play_circle_outline,
-                          ),
-                        ),
-                        IconButton(
-                          iconSize: 120,
-                          color: Theme.of(context).cardColor,
-                          onPressed: onRefreshPressed,
-                          icon: const Icon(Icons.refresh_outlined),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Pomodors",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                            ),
-                          ),
-                          Text(
-                            "$totalPomodoros",
-                            style: TextStyle(
-                              fontSize: 60,
-                              fontWeight: FontWeight.w600,
-                              color:
-                                  Theme.of(context).textTheme.headline1!.color,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 2,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
   }
 }
